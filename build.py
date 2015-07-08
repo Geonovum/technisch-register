7dd0e270c092e55b593b904ab3b7dd099de6c74d
Simeon Nedkov <simeon@ndkv.nl>
2015-07-08 11:16:21 +0200

    add top10nl

--- build.py
+++ build.py
@@ -11,250 +11,250 @@ import codecs
 import time
 
 def create_standard_title(title, description):
-	title = '''
-		<h2>%s</h2>
-		<p>%s</p>
-		''' % (title, description)
+    title = '''
+        <h2>%s</h2>
+        <p>%s</p>
+        ''' % (title, description)
 
-	# use python's built-in parser which skips
-	# creation of html and body tags
-	return BS(title, 'html.parser')
+    # use python's built-in parser which skips
+    # creation of html and body tags
+    return BS(title, 'html.parser')
 
 def create_substandard_title(standard, artifact, title):
-	title = '''
-		<p><i class="fa fa-file-o"></i>
-			<span style='margin-left: 25px'>
-				<a href="http://register.geostandaarden.nl/%s/%s">%s</a>
-			</span>
-		</p> ''' % (artifact, standard, title)
+    title = '''
+        <p><i class="fa fa-file-o"></i>
+            <span style='margin-left: 25px'>
+                <a href="http://register.geostandaarden.nl/%s/%s">%s</a>
+            </span>
+        </p> ''' % (artifact, standard, title)
 
-	return BS(title, 'html.parser')
+    return BS(title, 'html.parser')
 
 def create_substandard_description(substandard):
-	summary ='''
-		<p>
-			<span style='margin-left:37px; width: 100%%'>%s</span>
-		</p>
-		''' % substandard['beschrijving']
+    summary ='''
+        <p>
+            <span style='margin-left:37px; width: 100%%'>%s</span>
+        </p>
+        ''' % substandard['beschrijving']
 
-	return BS(summary, 'html.parser')
+    return BS(summary, 'html.parser')
 
 def create_standard_webpage(standard, artifacts):
-	# builds each standard's overview page
-	# e.g. http://register.geostandaarden.nl/imgeo/
+    # builds each standard's overview page
+    # e.g. http://register.geostandaarden.nl/imgeo/
 
-	# load standard HTML template
-	with open('web/templates/standard.html', 'r') as f:
-		html = BS(f)
+    # load standard HTML template
+    with open('web/templates/standard.html', 'r') as f:
+        html = BS(f)
 
-	# construct title
-	title = create_standard_title(standard['title'], standard['beschrijving'])
-	
-	# add to #title div
-	el_title = html.find(id="title")
+    # construct title
+    title = create_standard_title(standard['title'], standard['beschrijving'])
+    
+    # add to #title div
+    el_title = html.find(id="title")
 
-	# fetch #container from template
-	el_container = html.find(id="container")
+    # fetch #container from template
+    el_container = html.find(id="container")
 
-	# append title
-	el_title.append(title)
+    # append title
+    el_title.append(title)
 
-	with codecs.open('descriptions.json', encoding='utf8') as f:
-		descriptions = load(f)
+    with codecs.open('descriptions.json', encoding='utf8') as f:
+        descriptions = load(f)
 
-	# iterate over all artifacts i.e. informatiemodel, gmlapplicatieschema, regels, etc.
-	for artifact in artifacts:
-		# create title of each sub standard
-		title = create_substandard_title(standard['id'], artifact, descriptions[artifact]['titel'])
-		el_container.append(title)
+    # iterate over all artifacts i.e. informatiemodel, gmlapplicatieschema, regels, etc.
+    for artifact in artifacts:
+        # create title of each sub standard
+        title = create_substandard_title(standard['id'], artifact, descriptions[artifact]['titel'])
+        el_container.append(title)
 
-		# create description of each standard
-		description = create_substandard_description(descriptions[artifact])
-		el_container.append(description)
+        # create description of each standard
+        description = create_substandard_description(descriptions[artifact])
+        el_container.append(description)
 
-	return html.prettify()
+    return html.prettify()
 
 def create_overview_entry(standard, description):
-	# url = "http://register.geostandaarden.nl"
-	url = "."
-	overview = '''
-		<p>
-			<i class="fa fa-file"></i>
-			<span style='margin-left: 25px'>
-				<a href="%s/%s/index.html">%s</a>
-			</span>
-		</p>
-		<p><span style='margin-left:37px; width: 100%%'>%s</span></p>
-			''' % (url, standard, standard.upper(), description)
+    # url = "http://register.geostandaarden.nl"
+    url = "."
+    overview = '''
+        <p>
+            <i class="fa fa-file"></i>
+            <span style='margin-left: 25px'>
+                <a href="%s/%s/index.html">%s</a>
+            </span>
+        </p>
+        <p><span style='margin-left:37px; width: 100%%'>%s</span></p>
+            ''' % (url, standard, standard.upper(), description)
 
- 	return BS(overview, 'html.parser')
+    return BS(overview, 'html.parser')
 
 def create_overview_page(standards, source, destination):
-	print 'Creating overview page...'
+    print 'Creating overview page...'
 
-	# open overview page template
-	with codecs.open('web/templates/overview.html', 'r', encoding='utf8') as f:
-		html = BS(f)
+    # open overview page template
+    with codecs.open('web/templates/overview.html', 'r', encoding='utf8') as f:
+        html = BS(f)
 
-	el_container = html.find(id='leftcolumn')
+    el_container = html.find(id='leftcolumn')
 
-	for standard in standards:
-		overview = create_overview_entry(standard['id'], standard['beschrijving'])
-		el_container.append(overview)
+    for standard in standards:
+        overview = create_overview_entry(standard['id'], standard['beschrijving'])
+        el_container.append(overview)
 
-	with codecs.open('%s/index.html' % destination, 'w', encoding='utf8') as f:
-		f.write(html.prettify())
-		#OSFS('./').copydir('../web/assets', '%s/assets' % destination)
-		call('cp -r web/assets %s/assets' % destination, shell=True)
+    with codecs.open('%s/index.html' % destination, 'w', encoding='utf8') as f:
+        f.write(html.prettify())
+        #OSFS('./').copydir('../web/assets', '%s/assets' % destination)
+        call('cp -r web/assets %s/assets' % destination, shell=True)
 
-		print 'Done!'
+        print 'Done!'
 
 def build_folders(source, destination, standards, root):
-	print "Building register..."
+    print "Building register..."
 
-	source_fs = OSFS(source)
+    source_fs = OSFS(source)
 
-	# iterate over all standards in source directory
-	for standard in standards:
-		print "Processing %s ... " % standard['id']
-		standard_fs = source_fs.opendir(standard['id'])
+    # iterate over all standards in source directory
+    for standard in standards:
+        print "Processing %s ... " % standard['id']
+        standard_fs = source_fs.opendir(standard['id'])
 
-		# list all sub standards of a standard
-		artifacts = standard_fs.listdir(dirs_only=True)
-		if '.git' in artifacts: artifacts.remove(".git")
+        # list all sub standards of a standard
+        artifacts = standard_fs.listdir(dirs_only=True)
+        if '.git' in artifacts: artifacts.remove(".git")
 
-		for artifact in artifacts:
-			# check whether artifact folder exists in destination 
-			if root.exists('%s/%s' % (destination, artifact)) == False:
-				root.makedir('%s/%s' % (destination, artifact))
-				
-			# copy standard folders from source to destination in desired structure
-			root.copydir('%s/%s/%s' % (source, standard['id'], artifact),  '%s/%s/%s' % (destination, artifact, standard['id']))
+        for artifact in artifacts:
+            # check whether artifact folder exists in destination 
+            if root.exists('%s/%s' % (destination, artifact)) == False:
+                root.makedir('%s/%s' % (destination, artifact))
+                
+            # copy standard folders from source to destination in desired structure
+            root.copydir('%s/%s/%s' % (source, standard['id'], artifact),  '%s/%s/%s' % (destination, artifact, standard['id']))
 
-		# create standard HTML page
-		html = create_standard_webpage(standard, artifacts)
+        # create standard HTML page
+        html = create_standard_webpage(standard, artifacts)
 
-		# check whether standard folder exists in register root
-		if root.exists('%s/%s' % (destination, standard['id'])) == False:
-			root.makedir('%s/%s' % (destination, standard['id']))
-		
-		# write standard HTML page to register/standard/index.html
-		with codecs.open('%s/%s/index.html' % (destination, standard['id']), 'w', encoding='utf8') as f:
-			f.write(html)
+        # check whether standard folder exists in register root
+        if root.exists('%s/%s' % (destination, standard['id'])) == False:
+            root.makedir('%s/%s' % (destination, standard['id']))
+        
+        # write standard HTML page to register/standard/index.html
+        with codecs.open('%s/%s/index.html' % (destination, standard['id']), 'w', encoding='utf8') as f:
+            f.write(html)
 
 def fetch_repos(root, destination, repos):
-	print "Fetching repositories..."
+    print "Fetching repositories..."
 
-	for repo in repos:
-		print "Cloning %s in repos/%s" % (repo['url'], repo['id'])
-		# explicitely create dir as implicit cration fails on server
-		root.makedir('%s/%s' % (destination, repo['id']))
-		call('git clone %s repos/%s' % (repo['url'], repo['id']), shell=True)
+    for repo in repos:
+        print "Cloning %s in repos/%s" % (repo['url'], repo['id'])
+        # explicitely create dir as implicit cration fails on server
+        root.makedir('%s/%s' % (destination, repo['id']))
+        call('git clone %s repos/%s' % (repo['url'], repo['id']), shell=True)
 
-	#TODO: git pull additions into existing repos, clone new ones
+    #TODO: git pull additions into existing repos, clone new ones
 
 
 def run(status):
-	# environ['TR_RUNNING'] = 'true'
+    # environ['TR_RUNNING'] = 'true'
 
-	status['running'] = True
-	with open('status.json', 'w') as f:
-		f.write(dumps(status))
+    status['running'] = True
+    with open('status.json', 'w') as f:
+        f.write(dumps(status))
 
-	source = 'repos'
-	destination = 'register2'
+    source = 'repos'
+    destination = 'register2'
 
-	root = OSFS('./') # 'c:\Users\<login name>' on Windows
-	
-	try:
-		print "removing %s" % source
-		# removedir function cannot deal with protected
-		# files in each repo's .git folder
-		call('rm -rf %s' % source, shell=True)
-	except ResourceNotFoundError: 
-		print "Failed to remove %s..." % source
+    root = OSFS('./') # 'c:\Users\<login name>' on Windows
+    
+    try:
+        print "removing %s" % source
+        # removedir function cannot deal with protected
+        # files in each repo's .git folder
+        call('rm -rf %s' % source, shell=True)
+    except ResourceNotFoundError: 
+        print "Failed to remove %s..." % source
 
-	# try:
-	# 	print "removing %s" % destination
-	# 	root.removedir(destination, force=True)
-	# except ResourceNotFoundError:
-	# 	print "Failed to remove %s..." % destination
-	
-	root.makedir(source)
-	# chmod(source, S_IRWXU | S_IRWXG | S_IRWXO)
-	root.makedir(destination)
-	# chmod(destination, S_IRWXU | S_IRWXG | S_IRWXO)
+    # try:
+    #   print "removing %s" % destination
+    #   root.removedir(destination, force=True)
+    # except ResourceNotFoundError:
+    #   print "Failed to remove %s..." % destination
+    
+    root.makedir(source)
+    # chmod(source, S_IRWXU | S_IRWXG | S_IRWXO)
+    root.makedir(destination)
+    # chmod(destination, S_IRWXU | S_IRWXG | S_IRWXO)
 
-	#standards = OSFS(source).listdir(dirs_only=True)
-	with open('repos.json') as f:
-		standards = load(f)
-	
-	fetch_repos(root, destination, standards)
-	build_folders(source, destination, standards, root)
-	create_overview_page(standards, source, destination)
+    #standards = OSFS(source).listdir(dirs_only=True)
+    with open('repos.json') as f:
+        standards = load(f)
+    
+    fetch_repos(root, destination, standards)
+    build_folders(source, destination, standards, root)
+    create_overview_page(standards, source, destination)
 
-	print 'Copying register to staging...'
+    print 'Copying register to staging...'
 
-	call('rm -rf ../register/staging', shell=True)
+    call('rm -rf ../register/staging', shell=True)
 
 
-	call('mv %s ../register/staging' % destination, shell=True)
-	# root.copydir(destination, '../register/staging')
-	# root.removedir(destination, force=True)
-	
-	# call('rm -rf %s' % source, shell=True)
-	call('chmod -R a+rx ../register/staging', shell=True)
-	# root.removedir(source, force=True)
+    call('mv %s ../register/staging' % destination, shell=True)
+    # root.copydir(destination, '../register/staging')
+    # root.removedir(destination, force=True)
+    
+    # call('rm -rf %s' % source, shell=True)
+    call('chmod -R a+rx ../register/staging', shell=True)
+    # root.removedir(source, force=True)
 
-	with open('status.json') as f:
-		status = load(f)
+    with open('status.json') as f:
+        status = load(f)
 
-	if status['again'] == True:
-		status['again'] = False
+    if status['again'] == True:
+        status['again'] = False
 
-		with open('status.json', 'w') as f:
-			f.write(dumps(status))
+        with open('status.json', 'w') as f:
+            f.write(dumps(status))
 
-		print "Running once more..."
-		run(status)
-	else:
-		status['running'] = False
-		with open('status.json', 'w') as f:
-			f.write(dumps(status))
+        print "Running once more..."
+        run(status)
+    else:
+        status['running'] = False
+        with open('status.json', 'w') as f:
+            f.write(dumps(status))
 
-	# if environ['TR_AGAIN'] == 'true':
-	# 	environ['TR_AGAIN'] = 'false'
+    # if environ['TR_AGAIN'] == 'true':
+    #   environ['TR_AGAIN'] = 'false'
 
-	# 	print "Running once more..."
-	# 	run()
-	# else:
-	# 	environ['TR_RUNNING'] = 'false'
+    #   print "Running once more..."
+    #   run()
+    # else:
+    #   environ['TR_RUNNING'] = 'false'
 
 #if __name__ == "__main__":
 with open('status.json') as f:
-	status = load(f)
+    status = load(f)
 
 print "Content-Type: text/html"
 print 
 print "Running sync script..."
 
 if status['running'] == False:
-	status['running'] = True
-	run(status)
+    status['running'] = True
+    run(status)
 
 elif status['running'] == True:
-	status['again'] = True
+    status['again'] = True
 
-	with open('status.json', 'w') as f:
-		f.write(dumps(status))
+    with open('status.json', 'w') as f:
+        f.write(dumps(status))
 
 print "Queue: %s" % status['again']
 
-	# if 'TR_RUNNING' not in environ:
-	# 	environ['TR_RUNNING'] = 'true'
-	# 	run()
-	# elif environ['TR_RUNNING'] == 'false':
-	# 	environ['TR_RUNNING'] = 'true'
-	# 	run()
-	# elif environ['TR_RUNNING'] == 'true':
-	# 	environ['TR_AGAIN'] = 'true'
+    # if 'TR_RUNNING' not in environ:
+    #   environ['TR_RUNNING'] = 'true'
+    #   run()
+    # elif environ['TR_RUNNING'] == 'false':
+    #   environ['TR_RUNNING'] = 'true'
+    #   run()
+    # elif environ['TR_RUNNING'] == 'true':
+    #   environ['TR_AGAIN'] = 'true'
