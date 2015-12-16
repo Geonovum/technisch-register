8a8dbb52791c09eec4734c4cff58e221ab07c514
NJBakker <nicojanbakker@gmail.com>
2015-12-16 18:01:34 +0100

    Clusters en clustering
    Modellen die niet onder een cluster vallen verschijnen nu ook in de
    mainpage.

--- backend.py
+++ backend.py
@@ -7,40 +7,54 @@ import codecs
 import time
 import logging
 
-def build_folders(source, destination_temp, standard, root):
-    """Transform the repos' folder structure to that of the register
-    and build HTML pages for each standard.
-    """
+def build_folders(source, destination_temp, standard, root, repoCluster):
+	"""Transform the repos' folder structure to that of the register
+	and build HTML pages for each standard.
+	"""
 
-    source_fs = OSFS(source)
+	source_fs = OSFS(source)
 
-    print "Processing %s ... " % standard['id']
-    standard_fs = source_fs.opendir(standard['id'])
+	print "Processing %s ... " % standard['id']
+	standard_fs = source_fs.opendir(standard['id'])
 
     # list all artifacts of a standard
-    artifacts = standard_fs.listdir(dirs_only=True)
-    if '.git' in artifacts: artifacts.remove(".git")
+	artifacts = standard_fs.listdir(dirs_only=True)
+	if '.git' in artifacts: artifacts.remove(".git")
 
-    for artifact in artifacts:
-        # check whether artifact folder exists in destination_temp 
-        if root.exists('%s/%s' % (destination_temp, artifact)) == False:
-            root.makedir('%s/%s' % (destination_temp, artifact))
+	for artifact in artifacts:
+		# check whether artifact folder exists in destination_temp 
+		if root.exists('%s/%s' % (destination_temp, artifact)) == False:
+			root.makedir('%s/%s' % (destination_temp, artifact))
 
-        # copy standard folders from source to destination_temp in desired structure
-        root.copydir('%s/%s/%s' % (source, standard['id'], artifact),  '%s/%s/%s' % (destination_temp, artifact, standard['id']))
+		# copy standard folders from source to destination_temp in desired structure
+		root.copydir('%s/%s/%s' % (source, standard['id'], artifact),  '%s/%s/%s' % (destination_temp, artifact, standard['id']))
 
-    html = create_standard_webpage(standard, artifacts)
+	html = create_standard_webpage(standard, artifacts)
 
-    # check whether register/standard exists
-    if root.exists('%s/%s' % (destination_temp, standard['id'])) == False:
-        root.makedir('%s/%s' % (destination_temp, standard['id']))
+	# check whether model is part of a cluster
+	if repoCluster == "":
+		# check whether register/standard exists
+		if root.exists('%s/%s' % (destination_temp, standard['id'])) == False:
+			root.makedir('%s/%s' % (destination_temp, standard['id']))
+		# write standard HTML page to register/standard/index.html
+		with codecs.open('%s/%s/index.html' % (destination_temp, standard['id']), 'w', encoding='utf8') as f:
+			f.write(html)		
+		
+	
+	else:
+		# check whether register/cluster/exists
+		if root.exists('%s/%s' % (destination_temp, repoCluster)) == False:
+			root.makedir('%s/%s' % (destination_temp, repoCluster))
+		# check whether register/cluster/standard exists
+		if root.exists('%s/%s/%s' % (destination_temp, repoCluster, standard['id'])) == False:
+			root.makedir('%s/%s/%s' % (destination_temp, repoCluster, standard['id']))
     
-    # write standard HTML page to register/standard/index.html
-    with codecs.open('%s/%s/index.html' % (destination_temp, standard['id']), 'w', encoding='utf8') as f:
-        f.write(html)
+		# write standard HTML page to register/cluster/standard/index.html
+		with codecs.open('%s/%s/%s/index.html' % (destination_temp, repoCluster, standard['id']), 'w', encoding='utf8') as f:
+			f.write(html)
 
     # copy web assets
-    root.copydir('web/assets', '%s/r' % destination_temp, overwrite=True)
+	root.copydir('web/assets', '%s/r' % destination_temp, overwrite=True)
 
 def fetch_repo(root, repo, url, destination_temp):
     """Clone repos from GitHub to source folder."""
