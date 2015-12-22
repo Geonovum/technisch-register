2e446467dc9497cfabef439d27bfeec538e6e9de
Simeon Nedkov <simeon@ndkv.nl>
2015-12-22 16:45:06 +0100

    convert tabs to spaces

--- build.py
+++ build.py
@@ -12,12 +12,12 @@ print
 print "Starting sync script..."
 
 if 'verbose' in argv:
-	Popen(['/usr/bin/python', 'run.py'], stdin=stdin)	
+    Popen(['/usr/bin/python', 'run.py'], stdin=stdin)
 else:
-	with open(devnull, 'w') as fp:
-		# directing stderr to stdout is essential
-		# otherwise script waits for Popen to finish
-		Popen(['/usr/bin/python', 'run.py'], stdin=stdin, stdout=fp, stderr=STDOUT)
+    with open(devnull, 'w') as fp:
+        # directing stderr to stdout is essential
+        # otherwise script waits for Popen to finish
+        Popen(['/usr/bin/python', 'run.py'], stdin=stdin, stdout=fp, stderr=STDOUT)
 
 print "Check http://register.geostandaarden.nl/log.txt for a status report."
 
