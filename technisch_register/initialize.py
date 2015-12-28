83f37333d51d01d5fe8d4c3642466667871b61e0
Simeon Nedkov <simeon@ndkv.nl>
2015-12-28 16:11:10 +0100

    use better var names

--- technisch_register/initialize.py
+++ technisch_register/initialize.py
@@ -1,14 +1,14 @@
 from fs.osfs import OSFS
-from settings import deploy_path, source, staging_build, production_build, backups, repo_path
+from settings import root_path, build_path, repos_path, sources_path, staging_path, register_path, backups_path
 from backend import fetch_repo
 from utils import load_repos
 
-root = OSFS('.')
-deploy = root.makeopendir(deploy_path)
-deploy.makedir(source)
-deploy.makedir(staging_build)
-deploy.makedir(production_build)
-deploy.makedir(backups)
+root = OSFS(root_path)
+build = root.makeopendir(build_path)
+build.makedir(sources_path)
+build.makedir(staging_path)
+build.makedir(register_path)
+build.makedir(backups_path)
 
-for repo in load_repos(repo_path).values(): 
-    fetch_repo(deploy, source, repo['id'], repo['url'])
+for repo in load_repos(repos_path).values(): 
+    fetch_repo(root, sources_path, repo['id'], repo['url'])
