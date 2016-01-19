2abfd961d9e5a8c6f63cdff52a414d42ab3c4536
Simeon Nedkov <simeon@ndkv.nl>
2016-01-19 10:22:49 +0100

    create production folder

--- technisch_register/initialize.py
+++ technisch_register/initialize.py
@@ -1,18 +1,25 @@
 from fs.osfs import OSFS
-from settings import root_path, build_path, repos_path, sources_path, staging_path, register_path, backups_path
+from fs.errors import ResourceNotFoundError
+import settings as s
 from backend import fetch_repo
 from utils import load_repos
 
-root = OSFS(root_path)
-build = root.makeopendir(build_path)
-build.makedir(sources_path)
-build.makedir(staging_path)
-build.makedir(register_path)
-build.makedir(backups_path)
+root = OSFS(s.root_path)
+build = root.makeopendir(s.build_path)
+build.makedir(s.sources_path)
+build.makedir(s.staging_path)
+build.makedir(s.register_path)
+build.makedir(s.backups_path)
 
-# TODO: make the production folder if does not exist
+# create production directory
+try:
+    OSFS(s.production_path)
+except ResourceNotFoundError:
+    path = s.production_path.split('/')[-2]
+    OSFS(s.production_path[:len(s.production_path) - (len(path) + 1)]).makedir(path)
 
-for repo in load_repos(repos_path).values(): 
+# fetch repos from GitHub
+for repo in load_repos(s.repos_path).values():
     fetch_repo(root, sources_path, repo['id'], repo['url'])
 
 # TODO: build the register once
