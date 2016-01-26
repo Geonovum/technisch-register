a0725d14eb48f1eb27e3955f39fe24fac0e45028
Simeon Nedkov <simeon@ndkv.nl>
2016-01-26 18:32:18 +0100

    fix webpages import for Travis CI

--- technisch_register/backend.py
+++ technisch_register/backend.py
@@ -2,7 +2,7 @@ from fs.osfs import OSFS
 from fs.errors import ResourceNotFoundError
 from subprocess import call
 # from webpages import create_standard_webpage
-import webpages
+import technisch_register.webpages
 from settings import build_path, repos_path, assets_path, cluster_path
 from os import path as ospath
 from utils import get_artifacts, load_repos, cleanup
