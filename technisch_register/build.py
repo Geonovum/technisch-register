edd290abc658761129b93ff51d582163f40ed3ed
Simeon Nedkov <simeon@ndkv.nl>
2017-09-27 11:20:29 +0200

    parametrise python path to allow for virtualenv runs

--- technisch_register/build.py
+++ technisch_register/build.py
@@ -3,7 +3,7 @@
 from subprocess import Popen, STDOUT
 from sys import stdin, exit, argv
 from os import devnull, path, chdir
-from settings import root_path
+from settings import root_path, python_path
 
 # VERSION = '0.5.0'
 
@@ -14,13 +14,13 @@ print "Starting sync script..."
 chdir(root_path)
 
 if 'verbose' in argv:
-    Popen(['/usr/bin/python', 'run.py'], stdin=stdin)
+    Popen([python_path, 'run.py'], stdin=stdin)
 else:
     with open('output.txt', 'a') as fp:
         # directing stderr to stdout is essential
         # otherwise script waits for Popen to finish
         # and causes request to timeout
-        Popen(['/usr/bin/python', path.join(root_path, 'run.py')], stdin=stdin, stdout=fp, stderr=STDOUT)
+        Popen([python_path, path.join(root_path, 'run.py')], stdin=stdin, stdout=fp, stderr=STDOUT)
 
 print "Check https://register.geostandaarden.nl/log.txt for a status report."
 
