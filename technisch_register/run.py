1cd9bfa2e9a0f0da3f66e2a60799c2d7010779bc
Simeon Nedkov <simeon@ndkv.nl>
2017-09-27 13:43:12 +0200

    major refactoring: remove stagng completely, fetch paths from settings

--- technisch_register/run.py
+++ technisch_register/run.py
@@ -10,24 +10,15 @@ import logging
 import settings
 from queue import FifoSQLiteQueue
 
-root_path = settings.root_path
-root = OSFS(root_path)
 repos_path = settings.repos_path
-source = settings.sources_path
 staging_build = settings.staging_path
-backups = settings.backups_path
 build_path = settings.build_path
-register_path = settings.register_path
-script_entry_path = settings.script_entry_path
-production_path = settings.production_path
-assets_path = settings.assets_path
 
 logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 print "\nRunning on %s" % datetime.datetime.now()
 
 # read GitHub's JSON POST payload from stdin
-# see ./github-payload.json for an example
 payload = load(stdin)
 
 try:
@@ -37,18 +28,14 @@ except KeyError:
     logging.error("This payload does not carry a release... aborting.")
     exit()
 
-# extract the release type: release or prerelease
 initiator = payload['repository']['name'].lower()
 
 queue = FifoSQLiteQueue('queue.db')
 
 if action == 'published':
-    # check whether we can start the build.py script
-    # i.e. whether another instance isn't already running
-    # TODO: rename run to sync_script_running
     if not build_script_running():
-            backend.build_register(source, register_path, root, initiator)
-            backend.deploy_production(register_path, backups, script_entry_path, production_path)
+            backend.build_register(initiator)
+            backend.deploy_register()
     else:
         queue.push(initiator)
         exit()
@@ -56,7 +43,7 @@ if action == 'published':
 while len(queue) > 0:
     initiator = queue.pop()
 
-    backend.build_register(source, register_path, root, initiator)
-    backend.deploy_production(register_path, backups, script_entry_path, production_path)
+    backend.build_register(initiator)
+    backend.deploy_register()
 
 queue.close()
