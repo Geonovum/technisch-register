781e6fb4a7ea69c7d065faef91f0227f9232639e
Simeon Nedkov <simeon@ndkv.nl>
2017-09-29 14:12:33 +0200

    add cgi-bin.py as a file instead of symbolic link to build.py

--- technisch_register/cgi-bin.py
+++ technisch_register/cgi-bin.py
@@ -1,1 +1,14 @@
-../technisch_register/build.py
+from subprocess import Popen
+from sys import stdin
+import json
+
+payload = json.load(stdin)
+prerelease = payload['release']['prerelease']
+
+if prerelease:
+    environment = 'staging'
+else:
+    environment = 'production'
+
+path = '/var/www/geostandaarden/{}/technisch-register/technisch_register/build.py'.format(environment)
+Popen(['/usr/bin/python', path], stdin=stdin)
