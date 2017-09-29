2366ab91afc9982af493a32dfdf3b64585888bde
Simeon Nedkov <simeon@ndkv.nl>
2017-09-29 15:35:15 +0200

    remove write to output.txt as it may be opened twice if scripts gets queued

--- technisch_register/build.py
+++ technisch_register/build.py
@@ -2,7 +2,7 @@
 
 from subprocess import Popen, STDOUT
 from sys import stdin, exit, argv
-from os import devnull, path, chdir
+from os import path, chdir, devnull
 from settings import root_path, python_path
 
 # VERSION = '0.5.0'
@@ -16,12 +16,7 @@ chdir(root_path)
 if 'verbose' in argv:
     Popen([python_path, 'run.py'], stdin=stdin)
 else:
-    with open('output.txt', 'a') as fp:
-        # directing stderr to stdout is essential
-        # otherwise script waits for Popen to finish
-        # and causes request to timeout
-        Popen([python_path, path.join(root_path, 'run.py')], stdin=stdin, stdout=fp, stderr=STDOUT)
-
-print "Check https://register.geostandaarden.nl/log.txt for a status report."
-
-exit()
+    # directing stderr to stdout is essential
+    # otherwise script waits for Popen to finish
+    # and causes request to timeout
+    Popen([python_path, path.join(root_path, 'run.py')], stdin=stdin, stdout=devnull, stderr=STDOUT)
